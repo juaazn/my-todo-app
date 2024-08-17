ae759fa9257010d0aac352834b36cb635a5bc6e3
chuans <juaazn.05@gmail.com>
2024-08-17 15:21:36 +0200

    collection task

--- server/models/Task.js
+++ server/models/Task.js
@@ -2,7 +2,17 @@ import mongoose from 'mongoose'
 
 const TaskScheme = new mongoose.Schema(
   {
-    
+    tile: {
+      type: String,
+      required: [true, 'Campo requerido']
+    },
+    completed: {
+      type: Boolean,
+      default: false
+    }
+  },
+  {
+    timestamps: true
   }
 )
 

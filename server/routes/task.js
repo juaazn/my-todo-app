48f210be204b27084eacb0cfa0738e18a00b6ede
chuans <juaazn.05@gmail.com>
2024-08-18 17:34:43 +0200

    News end points

--- server/routes/task.js
+++ server/routes/task.js
@@ -4,8 +4,10 @@ import TaskControllers from '../controllers/TaskControllers.js'
 const task = express.Router()
 
 task.post('/create', TaskControllers.create)
+task.post('/update-title/:_id')
 task.put('/update/:_id', TaskControllers.update)
-task.delete('/delete/:id', TaskControllers.delete)
+task.delete('/delete/:_id', TaskControllers.delete)
 task.get('/', TaskControllers.getAll)
+task.get('/:id', TaskControllers.getAll)
 
 export default task

e523592652a981d050e1b9356b1e934d480448e2
chuans <juaazn.05@gmail.com>
2024-08-18 13:30:03 +0200

    router CRUD

--- server/routes/task.js
+++ server/routes/task.js
@@ -4,5 +4,8 @@ import TaskControllers from '../controllers/TaskControllers.js'
 const task = express.Router()
 
 task.post('/create', TaskControllers.create)
+task.put('/update/:_id', TaskControllers.update)
+task.delete('/delete/:id', TaskControllers.delete)
+task.get('/', TaskControllers.getAll)
 
 export default task

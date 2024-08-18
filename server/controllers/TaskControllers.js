7b5de372580ccb44f3e5e94cd247af71dc300b8e
chuans <juaazn.05@gmail.com>
2024-08-18 17:34:16 +0200

    Change status code

--- server/controllers/TaskControllers.js
+++ server/controllers/TaskControllers.js
@@ -4,27 +4,25 @@ const TaskControllers = {
   async create (req, res) {
     try {
       const task = await Tasks.create(req.body)
-      res.status(200).send(task)
+      res.status(201).send(task)
     } catch (error) {
-      res.status(500).send({ message: 'Error server Something went wrong while creating the task', error })
+      res.status(500).send({ message: 'Server error Something went wrong while creating the task', error })
     }
   },
   async update (req, res) {
     try {
       await Tasks.findByIdAndUpdate(req.params._id, req.body, { new: true })
-      res.status(201).send({ message: 'Task update' })
+      res.status(200).send({ message: 'Task update' })
     } catch (error) {
-      res.status(500).send({ message: 'Error server Something went wrong while updating the task', error })
+      res.status(500).send({ message: 'Server error Something went wrong while updating the task', error })
     }
   },
   async delete (req, res) {
     try {
-      const _id = req.params._id
-
-      await Tasks.findByIdAndDelete(_id)
+      await Tasks.findByIdAndDelete(req.params._id)
       res.status(204).send({ message: 'task deleted' })
     } catch (error) {
-      res.status(500).send({ message: 'Error server algo ha fallado al eliminar la tarea', error })
+      res.status(500).send({ message: 'Server error something went wrong while deleting the task', error })
     }
   },
   async getAll (req, res) {
@@ -32,7 +30,22 @@ const TaskControllers = {
       const allTask = await Tasks.find()
       res.status(200).send(allTask)
     } catch (error) {
-      res.status(500).send({ message: 'Error server algo ha fallado al obtener todas las tareas', error })
+      res.status(500).send({ message: 'Server error something went wrong while getting all tasks', error })
+    }
+  },
+  async updateTile () {
+    try {
+      await Tasks.findByIdAndUpdate(req.params._id, { title: req.body.title }, { new: true })
+    } catch (error) {
+      res.status(500).send({ message: 'Server error updateTitle', error })
+    }
+  },
+  async getTaskForId (req, res) {
+    try {
+      const taskForId = await Tasks.findById(req.params._id)
+      res.status(200).send({ message: taskForId })
+    } catch (error) {
+      res.status(500).send({ message: 'Server error updateTitle', error })
     }
   }
 }

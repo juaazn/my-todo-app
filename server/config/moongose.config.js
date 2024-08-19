1c0e68e579ec7f57890465ced00f2e7207276373
chuans <juaazn.05@gmail.com>
2024-08-19 13:08:07 +0200

    new config mongoose

--- server/config/moongose.config.js
+++ server/config/moongose.config.js
@@ -3,7 +3,10 @@ import 'dotenv/config'
 
 export default async function dbConnection () {
   try {
-    await mongoose.connect(process.env.MONGO_URI)
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
     console.log('✅ Database connected successfully')
   } catch (err) {
     console.error(err)

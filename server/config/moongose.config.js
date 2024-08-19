1d2a29425641258ec2d51c2468956f9b8a3c5352
chuans <juaazn.05@gmail.com>
2024-08-19 13:23:57 +0200

    delte options

--- server/config/moongose.config.js
+++ server/config/moongose.config.js
@@ -3,10 +3,7 @@ import 'dotenv/config'
 
 export default async function dbConnection () {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    })
+    await mongoose.connect(process.env.MONGO_URI)
     console.log('✅ Database connected successfully')
   } catch (err) {
     console.error(err)
